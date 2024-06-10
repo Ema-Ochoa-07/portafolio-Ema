21884dc9ce21bd1f48b955a1a1c97e6e12a41452
Enmanuel Ochoa <163493441+Ema-Ochoa-07@users.noreply.github.com>
2024-06-10 13:06:37 -0500

    Dark Mode 1Parte

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,30 +50,10 @@ document.getElementById('contact-form')
 });
 
 
+const toogle_moon = document.querySelector('.toogle__moon')
+const body = document.querySelector('body')
 
-/*=============== DARK LIGTH MODE===============*/
-
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme ='ri-sun-line'
-
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark': 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line': 'ri-sun-line'
-
-if(selectedTheme){
-    themeButton.document.body.classList[selectedTheme === 'dark' ? 'add': 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'bx bsx-moon' ? 'add': 'remove'](iconTheme)
-
-    addEventListener('click', () =>{
-        document.body.classList.toggle(darkTheme)
-        themeButton.classList.toggle(iconTheme)
-
-        localStorage.setItem('selected-theme', getCurrentTheme())
-        localStorage.setItem('selected-icon', getCurrentIcon())
-    }
-)
+toogle_moon.onclick = function(){
+    toogle_moon.classList.toggle('active')
+    body.classList.toggle('active')
 }
